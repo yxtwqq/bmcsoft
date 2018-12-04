32fa1b70a7df2995c0542de9ba1925bf5c318882
yangxitao <1250896030@qq.com>
2018-12-04 11:31:27 +0800

    跨域的过滤器配置

--- src/main/java/cn/bmc/Application.java
+++ src/main/java/cn/bmc/Application.java
@@ -1,7 +1,13 @@
 package cn.bmc;
 
+import cn.bmc.filter.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +15,21 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @RequestMapping(value = "/", produces = "text/plain;charset=UTF-8")
+    public String index() {
+        return "Hello Spring Boot!";
+    }
+
+    @Bean
+    public FilterRegistrationBean securityFilter() {
+        CorsFilter filter = new CorsFilter();
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.addUrlPatterns("/*");
+        registration.setName("JSCrossDomainFilter");
+        return registration;
+    }
+
+
 }
